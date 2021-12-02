d972689b9a1815960f13989f024c8427faed3b83
Tower95 <torrestmi3@gmail.com>
2021-12-01 20:48:52 -0700

    adding components task / remove gitkeep

---
+++ src/components/task/index.js
@@ -0,0 +1,24 @@
+'use strict'
+const router = require('express').Router();
+
+router.get('/',(req,res)=>{
+
+});
+
+router.get('/',(req,res)=>{
+
+});
+
+router.post('/',(req,res)=>{
+
+});
+
+router.put('/',(req,res)=>{
+
+});
+
+router.delete('/',(req,res)=>{
+
+});
+
+module.exports = router;
