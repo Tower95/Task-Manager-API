4eae414de6714c76ec9027f10601e4a03d7ce978
Tower95 <torrestmi3@gmail.com>
2021-12-03 11:00:15 -0700

    installing docjs to generate doc

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict'
 //imports.
-const app = require('express')();
+var path = require('path');
+const express =require('express');
+const app = express();
 const bodyparser = require('./src/middleware').bodyparser;
 const validateJWT = require('./src/middleware/validateJWT').validateJWT
 
@@ -13,6 +15,7 @@ app.use(bodyparser.jsonParser);
 app.use(bodyparser.urlencodedParser); 
 app.use(validateJWT);
 
+
 //modules
 const task = require('./src/components/task').router;
 const user = require('./src/components/user').router;
@@ -20,7 +23,7 @@ const user = require('./src/components/user').router;
 //add routes
 app.use('/task', task);
 app.use('/user', user);
-
+app.use(express.static("doc"));
 
 //export express app.
 module.exports = app;
