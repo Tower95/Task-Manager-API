67e40dcf8ce95b0f5bee0421ecca736f47e82545
Tower95 <torrestmi3@gmail.com>
2021-12-02 18:38:12 -0700

    adding dal and controller user

--- src/components/user/controller.js
+++ src/components/user/controller.js
@@ -1,35 +1,132 @@
 'use strict'
 const router = require('express').Router();
+const User = require('./dal');
+const bcrypt = require('bcrypt');
+const { generateJWT } = require('../../utils/generateJWT');
 
-router.get('/', (req, res) => {
 
-  console.log(req.body);
-  res.json({ msg: '/ GET' });
+/**
+ * @example
+ * router.get('/', async (req, res) => {
+ * let { data }= req.body
+ * Logic....
+ * res.json({payload:"all good!"});
+ * }
+ */
+
+/**
+ * User Endpoint HTML GET ALL
+ */
+router.get('/', async (req, res) => {
+
+  let payload = undefined;
+  payload = await User.getAll();
+  if (!payload) {
+    return res.status(402).json({})
+  }
+  res.json({ payload });
 
 });
 
-router.get('/:id', (req, res) => {
+/**
+ * User Endpoint HTML GET ONE:ID
+ */
+router.get('/:id', async (req, res) => {
 
-  res.json({ msg: '/ GET :id' });
+  const { id } = req.params;
+
+  let user = await User.getById(Number(id));
+
+  if (!user) {
+    return res.status(404).json({ payload: "user doesn't exist" });
+  }
+
+  res.json({ payload: user });
 
 });
 
-router.post('/', (req, res) => {
+/**
+ * User Endpoint HTML POST CREATE 
+ */
+router.post('/signup', async (req, res) => {
+  let payload = undefined;
+  const body = req.body;
 
-  res.json({ msg: '/ POST' });
+  const salt = bcrypt.genSaltSync();
+  let password = bcrypt.hashSync(body.password, salt);
+
+  const user = {
+    name: body.name,
+    email: body.email,
+    role: body.role,
+    password
+  }
+
+  payload = await User.save(user);
+  res.json({ payload });
 
 });
 
-router.put('/:id', (req, res) => {
+/**
+ * User Endpoint HTML POST SIGNIN
+ */
+router.post('/signin', async (req, res) => {
+  let payload = undefined;
+  let { email, password } = req.body
+  let user = await User.getByEmail(email);
 
-  res.json({ msg: '/ PUT' });
+  if (!user) {
+    payload = { msg: `Can't find User` }
+    return res.status(401).json({ payload: payload });
+  }
+
+  const validPassword = await bcrypt.compareSync(password, user.password);
+
+  if (validPassword) {
+    let token = generateJWT({
+      name: user.name,
+      id: user.id
+    });
+
+    return res.status(200).json({ token });
+  }
+
+  payload = { msg: `Incorrect Credentials` }
+  return res.status(401).json({ payload: payload });
+});
+
+
+/**
+ * User Endpoint HTML PUT UPDATE ONE:ID
+ */
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, role } = req.body;
+  const data = { name, email, role}
+  let user = await User.updateById(Number(id), data);
+
+  if (!user) {
+    return res.status(404).json({ payload: "user doesn't exist" });
+  }
+
+  res.json({ payload: user });
 
 });
 
-router.delete('/:id', (req, res) => {
+/**
+ * User Endpoint HTML DELETE ONE:ID
+ */
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  let user = await User.deleteById(Number(id));
+  if (!user) {
+    return res.status(404).json({ payload: "user doesn't exist" });
+  }
 
-  res.json({ msg: '/ DELETE' });
-  
+  res.json({ payload: user });
+
 });
+
+
 
 module.exports = router;
--- src/components/user/dal.js
+++ src/components/user/dal.js
@@ -1,10 +1,116 @@
-const prisma = require('../../services/prisma');
+'use strict'
+const Prisma = require('../../services/prisma');
+const { handleError } = require('../../utils').ErrorHandel;
 
-exports.save = async(user)=>{
+
+/**
+ * @async
+ * @function Save a new User in the DB.
+ * @param {Object} user this has to contain all the scheme data.
+ * @returns {Object} User saved in the DB.
+ */
+exports.save = async (user) => {
   let result = undefined;
   try {
-    
-  } catch (error) {
-    console.log(error);
+    console.log(user);
+    result = await Prisma.user.create({ data: user });
   }
+  catch (error) {
+    handleError(error); 
+  }
+  return result;
+}
+
+/** 
+ * @async
+ * @function This function get all the Users in the DB.
+ * @returns Array Users
+ */
+exports.getAll = async () => {
+  let result = undefined;
+  try {
+    result = await Prisma.user.findMany();
+  }
+  catch (error) {
+    handleError(error);
+  }
+  return result;
+}
+
+/**
+ * @async
+ * @function get a User by Id.
+ * @param {Number} id 
+ * @returns User finded.
+ */
+exports.getById = async (id) => {
+  let result = undefined;
+  try {
+    result = await Prisma.user.findFirst({
+      where: { id }
+    });
+  }
+  catch (error) {
+    handleError(error);
+  }
+  return result;
+}
+
+/**
+ * @async
+ * @function get a User by email.
+ * @param {string} email
+ * @returns User finded
+ */
+ exports.getByEmail = async (email) => {
+  let result = undefined;
+  try {
+    result = await Prisma.user.findFirst({
+      where: { email }
+    });
+  }
+  catch (error) {
+    handleError(error);
+  }
+  return result;
+}
+
+/**
+ * @async
+ * @function update a User by his ID
+ * @param {Number} id 
+ * @param {Object} user new data
+ * @returns {Object} User modified in the DB,
+ */
+exports.updateById = async (id, user) => {
+  let result = undefined;
+  try {
+    result = await Prisma.user.update({
+      where: { id },
+      data:  user 
+    });
+  }
+  catch (error) {
+    handleError(error);
+  }
+  return result;
+}
+
+/**
+ * @async
+ * @function Delete forever a User on the DB
+ * @param {number} id 
+ * @returns the object deleted.
+ */
+exports.deleteById = async (id) => {
+  let result = undefined;
+  try {
+    result = await Prisma.user.delete({
+      where: {id}
+    })
+  } 
+  catch (error) {
+    handleError(error);  
+  }
+  return result;
 }
